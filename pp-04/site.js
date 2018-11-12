c29a22443af794ec7e66b9cbf81dc7e5b628da72
Anthea Gonzalez <agonzalez2@hawk.itt.edu>
2018-11-11 18:03:15 -0600

    Comment what I need to do but don't understand

--- pp-04/site.js
+++ pp-04/site.js
@@ -2,3 +2,9 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+// Add in event listener
+// document.getElementById("").addEventListner("click", function(){
+// alert("Hello");
+// });
+}
