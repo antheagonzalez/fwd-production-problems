a95a31e61228b61f6ed60ec4bf58549485c41010
Anthea Gonzalez <agonzalez2@hawk.itt.edu>
2018-11-16 20:41:55 -0600

    Made elf-executing

--- pp-05/site.js
+++ pp-05/site.js
@@ -17,11 +17,20 @@ console.log('The value of x is:', x, 'It should be 5.');
 //  namespace (e.g., neither its variables nor the double
 //  function can be accessed via the `window` global object,
 //  like `window.x`):
-
+// use old ?
 
 (function (){
   var x = 5;
+
+function double(num) {
+  var x = num * 2;
+  return x;
+
 }
+
+double(6);
+console.log('The value of x is:', x, 'It should be 5.');
+})();
 
 //  3. Correct this function so that there is no i variable in
 //  the global scope:
